9557129ffad3f3da80a49a6b286ce15498a36563
xiaohe <xiaohe198909@gmail.com>
2016-12-13 11:31:33 +0800

    Merge branches 'branches_1.0' and 'master' of https://github.com/xiaohegogo/el_test
    # Conflicts:
    #	src/main/java/MyTest.java

--- src/main/java/MyTest.java
+++ src/main/java/MyTest.java
@@ -5,8 +5,7 @@ public class MyTest {
 
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
-
-        System.out.println("new Branches");
+        System.out.println("master");
     }
 
 
