b7add48eda8f7beda7904c3a40e4a11c2aadc970
xiaohe <xiaohe198909@gmail.com>
2016-12-13 14:08:21 +0800

    Merge branch 'master' of https://github.com/xiaohegogo/el_test into branches_1.0
    # Conflicts:
    #	src/main/java/MyTest.java

--- src/main/java/MyTest.java
+++ src/main/java/MyTest.java
@@ -5,8 +5,8 @@ public class MyTest {
 
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
-        System.out.println("master");
-        System.out.println("merge from branch 1.0");
+        System.out.println("This is branch 2.0");
+        System.out.println("not merge ");
     }
 
 }
