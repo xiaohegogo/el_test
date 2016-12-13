bb7d441272c3b4af99a68eb5aee4f37fb72df1ff
xiaohe <xiaohe198909@gmail.com>
2016-12-13 11:01:38 +0800

    1.0

--- src/main/java/MyTest.java
+++ src/main/java/MyTest.java
@@ -6,7 +6,7 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
 
-        System.out.println("add");
+        System.out.println("This is branches 1.0");
     }
 
 
