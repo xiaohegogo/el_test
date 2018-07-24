5d720fc57c712879587016f86956b67b92cd832d
xiaohegogo <xiaohe198909@gmail.com>
2018-07-24 23:29:50 +0800

    Update MyTest.java

--- src/main/java/MyTest.java
+++ src/main/java/MyTest.java
@@ -3,6 +3,7 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
         System.out.println("master");
+        System.out.println("branch-02");
     }
 
 }
