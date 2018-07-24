a6c6b241010eb7a8994b0caedc513842c8f21c46
xiaohe <xiaohe198909@gmail.com>
2018-07-24 23:09:07 +0800

    update

--- src/main/java/MyTest.java
+++ src/main/java/MyTest.java
@@ -1,22 +1,8 @@
-/**
- * Created by hexiaolong1 on 2016/12/12.
- */
 public class MyTest {
 
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
         System.out.println("master");
-        System.out.println("==== branch 1.0 start ====");
-        System.out.println("The merge content is:");
-        StringBuilder sb = new StringBuilder();
-        sb.append("1").append("2");
-        System.out.println(sb.toString());
-        System.out.println("==== branch 1.0 end ====");
-        System.out.println("==== branch 2.0 start ====");
-        for (int i = 0; i < 2; i++) {
-            System.out.println("i:" + i);
-        }
-        System.out.println("==== branch 2.0 end ====");
     }
 
 }
